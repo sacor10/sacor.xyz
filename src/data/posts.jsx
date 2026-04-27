8175021228e97d2cd269b2c75b3fe889e17b82ad
Zach Andersen <sacor10@gmail.com>
2026-04-27 16:53:47 -0600

    Got it

--- src/data/posts.jsx
+++ src/data/posts.jsx
@@ -8,7 +8,7 @@ export const posts = [
     title: 'I Built a youtube link to mp4 downloader EXE and You Should Try It!!!',
     date: 'Posted April 14, 2026 by Sacor',
     excerpt:
-      'So I made this little tray app that just reminds you to drink water, but it yells at you in Comic Sans. Download it below and let me know how many hydration crimes it catches you committing!!! Haha jk, you should steal uncopyrighted music and videos and music videos from the internet with this, though.',
+      'So I made this little tray app that just reminds you to drink water, but it yells at you in Comic Sans. Download it below and let me know how many hydration crimes it catches you committing!!! Haha jk, you could steal uncopyrighted music and videos and music videos from the internet with this, though.',
     thumbBorder: '#FF00FF',
     thumb: '/placeholders/thumb1.svg',
     link: '/ytmp4',
@@ -18,7 +18,7 @@ export const posts = [
     title: 'Why Salt Lake City Is Basically a Giant LAN Party in the Mountains',
     date: 'Posted April 2, 2026 by Sacor',
     excerpt:
-      'A love letter to SLC gay bars, cold winters, and the people who keep nerding out with me about Theodore Roosevelt on the weekends. Also: I tried to LARP as a Utah College Republican. Results were mixed.',
+      'A love letter to SLUT, cold winters, and the people who are never nerding out with me about Theodore Roosevelt on the weekends. Also: I tried to LARP as a Utah College Republican. Results were mixed.',
     thumbBorder: '#00FFFF',
     thumb: '/placeholders/thumb2.svg',
     body: (
@@ -35,7 +35,7 @@ export const posts = [
     title: 'Ranking Every 90s Snack I Can Still Actually Find',
     date: 'Posted March 21, 2026 by Sacor',
     excerpt:
-      'Dunkaroos are BACK, Fruit Gushers never left, and I have STRONG opinions about Bagel Bites. Come fight me in the comments (that I haven\u2019t implemented yet).',
+      'Cheez-its are BACK, Fruit Gushers never left, and I have STRONG opinions about Bagel Bites. Come fight me in the comments (that I haven\u2019t implemented yet).',
     thumbBorder: '#00FF00',
     thumb: '/placeholders/thumb3.svg',
     body: (
