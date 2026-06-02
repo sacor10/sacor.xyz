b305c143755726762753224a03402566865aab1b
EASTON-RICKs <eastonricks16@gmail.com>
2026-06-02 23:12:46 +0000

    Easton

--- src/pages/EastonPage.jsx
+++ src/pages/EastonPage.jsx
@@ -16,8 +16,7 @@ const rightSidebar = (
             <br />
             <font face="Comic Sans MS" size="2" color="#00FF00">
               The Earth experiences over{' '}
-              <b className="yellow">500,000 earthquakes</b> per year — about
-              1,400 per day!
+              <b className="yellow">500,000 earthquakes</b> per year — about 1,400 per day!
             </font>
             <br />
             <br />
@@ -30,9 +29,7 @@ const rightSidebar = (
         </tr>
       </tbody>
     </table>
-
     <br />
-
     <table width="100%" cellPadding="8" cellSpacing="0" border="0" className="bevelbox">
       <tbody>
         <tr>
@@ -46,14 +43,10 @@ const rightSidebar = (
           <td bgcolor="#000000">
             <br />
             <font face="Courier New" size="2" color="#FFFF00">
-              <b className="cyan">M 1-2:</b> Barely felt
-              <br />
-              <b className="cyan">M 3-4:</b> Minor shaking
-              <br />
-              <b className="cyan">M 5-6:</b> Moderate damage
-              <br />
-              <b className="cyan">M 7+:</b> Major earthquake
-              <br />
+              <b className="cyan">M 1-2:</b> Barely felt <br />
+              <b className="cyan">M 3-4:</b> Minor shaking <br />
+              <b className="cyan">M 5-6:</b> Moderate damage <br />
+              <b className="cyan">M 7+:</b> Major earthquake <br />
               <b className="cyan">M 9+:</b>{' '}
               <font color="#FF00FF">MEGA quake!!</font>
             </font>
@@ -77,24 +70,46 @@ const mainContent = (
         <span className="blink">~ Welcome to your very own page! ~</span>
       </font>
     </center>
-
     <br />
+
+    {/* Big Goldendoodle Picture Container */}
+    <center>
+      <div style={{ 
+        border: '4px outset #00FFFF', 
+        padding: '10px', 
+        background: '#000000', 
+        maxWidth: '500px',
+        marginBottom: '20px'
+      }}>
+        <img 
+          src="/goldendoodle.jpg" 
+          alt="Happy Goldendoodle" 
+          style={{ 
+            width: '100%', 
+            height: 'auto', 
+            display: 'block',
+            border: '2px solid #FF00FF' 
+          }} 
+        />
+        <br />
+        <font face="Comic Sans MS" size="2" color="#FF00FF">
+          🐾 <b>GOLDENDOODLE POWER!</b> 🐾
+        </font>
+      </div>
+    </center>
 
     <font face="Comic Sans MS" size="3" color="#FFFFFF">
       Hey Easton! This page was made{' '}
-      <b className="hotpink">just for you</b>. You are awesome and the internet
-      has now been officially notified. Check out the cool earthquake stuff below!
+      <b className="hotpink">just for you</b>. You are awesome and the internet has now been officially notified. Check out the cool earthquake stuff below!
     </font>
-
     <br />
     <br />
-
     <table width="100%" cellPadding="8" cellSpacing="0" border="0" className="postbox">
       <tbody>
         <tr>
           <td align="center" bgcolor="#FF00FF" className="section-bar-sm">
             <font face="Impact" size="4" color="#FFFF00">
-              &#128200; EARTHQUAKE CENTRAL &#128200;
+              📈 EARTHQUAKE CENTRAL 📈
             </font>
           </td>
         </tr>
@@ -102,26 +117,19 @@ const mainContent = (
           <td align="center" bgcolor="#000000">
             <br />
             <font face="Comic Sans MS" size="3" color="#FFFFFF">
-              The <b className="cyan">U.S. Geological Survey</b> tracks every
-              earthquake happening on Earth — right now, in real time!
+              The <b className="cyan">U.S. Geological Survey</b> tracks every earthquake happening on Earth — right now!
             </font>
             <br />
             <br />
             <font face="Courier New" size="4" color="#00FF00">
-              <a
-                href="https://earthquake.usgs.gov"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="yellow-link"
-              >
+              <a href="https://usgs.gov" target="_blank" rel="noopener noreferrer" className="yellow-link" >
                 earthquake.usgs.gov
               </a>
             </font>
             <br />
             <br />
             <font face="Comic Sans MS" size="2" color="#FFFF00">
-              Click the link above to see live earthquake maps, recent quake
-              lists, and tons of science. The ground is always moving somewhere!
+              Click the link above to see live earthquake maps, recent quake lists, and tons of science. The ground is always moving somewhere!
             </font>
             <br />
             <br />
@@ -129,13 +137,11 @@ const mainContent = (
         </tr>
       </tbody>
     </table>
-
     <br />
     <br />
-
     <center>
       <font face="Comic Sans MS" size="2" color="#00FFFF">
-        &#9733; Stay curious, Easton! &#9733;
+        ★ Stay curious, Easton! ★
       </font>
     </center>
   </>
